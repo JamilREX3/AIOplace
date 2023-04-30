4e78a32d0c6eb938e64034eb55728ea485860b64
JamilREX3 <hamadt604@gmail.com>
2023-04-30 22:26:18 +0300

    HARK555

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,23 +1,16 @@
 const ApiError = require("../utils/apiError");
 
-const globalError = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
-  console.log(process.env.NODE_ENV);
-  if (process.env.NODE_ENV === "development") {
-    sendErrorForDev(err, res);
-  } else {
-    if (err.name === "JsonWebTokenError") err = handleJwtInvalidSignature();
-    if (err.name === "TokenExpiredError") err = handleJwtExpired();
-
-    sendErrorForProd(err, res);
-  }
-};
-
 const handleJwtExpired = () =>
   new ApiError("Expired token, please login again", 401);
 const handleJwtInvalidSignature = () =>
   new ApiError("Invalid token, please login again", 401);
+
+const sendErrorForProd = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+};
 
 const sendErrorForDev = (err, res) => {
   res.status(err.statusCode).json({
@@ -28,11 +21,18 @@ const sendErrorForDev = (err, res) => {
   });
 };
 
-const sendErrorForProd = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+const globalError = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+  //console.log(process.env.NODE_ENV);
+  if (process.env.NODE_ENV === "development") {
+    sendErrorForDev(err, res);
+  } else {
+    if (err.name === "JsonWebTokenError") err = handleJwtInvalidSignature();
+    if (err.name === "TokenExpiredError") err = handleJwtExpired();
+
+    sendErrorForProd(err, res);
+  }
 };
 
 module.exports = globalError;
