9bc60c8b3d9b9368500002980b4b1ae3e99f5b95
JamilREX3 <hamadt604@gmail.com>
2023-04-30 21:11:30 +0300

    second commit22

--- server.js
+++ server.js
@@ -29,7 +29,8 @@ app.use(compression());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "uploads")));
 
-if (process.env.NODE_ENV ==yapp.use(morgan("dev"));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
   console.log(`mode : ${process.env.NODE_ENV}`);
 }
 
