13f048c5bf9caeefaea333afbc329d12fd3ec1ee
JamilREX3 <hamadt604@gmail.com>
2023-05-01 17:23:20 +0300

    HARK2000

--- middlewares/uploadImageMiddleware.js
+++ middlewares/uploadImageMiddleware.js
@@ -1,18 +1,20 @@
-const ApiError = require("../utils/apiError");
 const multer = require("multer");
+// eslint-disable-next-line import/no-extraneous-dependencies
 
+const ApiError = require("../utils/apiError");
 
-const multerOptions = ()=>{
-    const multerStorage = multer.memoryStorage();
-    const multerFilter = function (req, file, cb) {
-        if (file.mimetype.startsWith("image")) {
-            cb(null, true);
-        } else {
-            cb(new ApiError("Only images Allowed", 400), false);
-        }
-    };
-    return multer({storage: multerStorage, fileFilter: multerFilter});
-}
+const multerOptions = () => {
+  const multerStorage = multer.memoryStorage();
+  const multerFilter = function (req, file, cb) {
+    if (file.mimetype.startsWith("image")) {
+      cb(null, true);
+    } else {
+      cb(new ApiError("Only images Allowed", 400), false);
+    }
+  };
+  return multer({ storage: multerStorage, fileFilter: multerFilter });
+};
 
-exports.uploadSingleImage = (fieldName)=>multerOptions().single(fieldName);
-exports.uploadMixOfImages = (arrayOfFields)=> multerOptions().fields(arrayOfFields);
+exports.uploadSingleImage = (fieldName) => multerOptions().single(fieldName);
+exports.uploadMixOfImages = (arrayOfFields) =>
+  multerOptions().fields(arrayOfFields);
